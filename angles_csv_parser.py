015432a8bd362041dd4752af0e036dc23e4fa509
andresg8 <43553155+andresg8@users.noreply.github.com>
2018-10-02 22:31:50 -0700

    Update angles_csv_parser.py

--- angles_csv_parser.py
+++ angles_csv_parser.py
@@ -2,16 +2,17 @@ import csv
 import urllib.request
 import math
 
-url =  'http://rapid-hub.org/data/angles_UCI_CS.csv'
-accessible = False
-try:
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-    accessible = True
-except:
-    print("url: " + url + " currently unreachable")
+def fetch_data() -> ".csv":
+    url =  'http://rapid-hub.org/data/angles_UCI_CS.csv'
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+        return data
+    except:
+        print("url: " + url + " currently unreachable")
 
-if accessible:
+def print_data(data: ".csv"):
+    if not data: return
     data = data.decode('utf-8')
     lines = data.split('\r\n')
     first = lines[0].split(',')
@@ -21,3 +22,14 @@ if accessible:
         if len(values) != 2:
             break
         print('{}\t\t{}\t\t{:.3f}'.format(values[0], values[1], math.cos(math.radians(int(values[1])))))
+
+if __name__ == "__main__":
+    data = fetch_data()
+    print_data(data)
+
+
+
+#pytests:
+
+def test_fetch_data():
+    assert type(fetch_data()) == bytes
